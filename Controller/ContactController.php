0ac5af1210964edd4fd1fd7ea5788e35b9ca404b
kapil <dev8484@gmail.com>
2015-06-15 16:39:46 +0530

    Add Validations

--- Controller/ContactController.php
+++ Controller/ContactController.php
@@ -2,6 +2,9 @@
 namespace Kap\ContactBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
 
 class ContactController extends Controller
 {
@@ -9,33 +12,50 @@ class ContactController extends Controller
         
         // CREATE CONTACT US FORM
         $form = $this->createFormBuilder()
-            ->add('name', 'text')
-            ->add('email', 'text')
-            ->add('subject', 'text')
-            ->add('message', 'textarea')
+            ->add('name', 'text',array(
+                'constraints' => array(
+                    new NotBlank(array('message' => 'Please enter name')),
+                    new Length(array('min' => 2,'minMessage' => 'Name should contain atleast 2 characters'))
+                ), 
+            ))
+            ->add('email', 'text',array(
+                'constraints' => array(
+                    new NotBlank(array('message' => 'Please enter email')),
+                    new Email(array('message' => 'Please enter valid email'))
+                )
+            ))
+            ->add('subject', 'text', array(
+                'constraints' => array(
+                    new NotBlank(array('message' => 'Please enter subject')),
+                ),
+            ))
+            ->add('message', 'textarea',array(
+                'constraints' => array(
+                    new NotBlank(array('message' => 'Please enter message')),
+                )
+            ))
             ->add('save', 'submit', array('label' => 'Send'))
             ->getForm(); 
         
-      if ($request->getMethod() == "POST") {
-            $form->handleRequest($request);
-            if ($form->isValid()) {
-                
-                // SEND EMAIL TO AUTHORIZE PERSON
-                $formData = $form->getData();
-               
-                $contactUsEmail = \Swift_Message::newInstance()
-                        ->setSubject("Contact Us: ".$formData['subject'])
-                        ->setFrom($formData['email'])
-                        ->setTo($this->container->getParameter('contact_us_email'))
-                        ->setBody($formData['message'])
-                        ->setContentType('text/html');
-                $this->container->get('mailer')->send($contactUsEmail);
-                
-                // REDIRECT TO CONTACT US PAGE
-                $this->get('session')->getFlashBag()->add('success', 'Your request has been successfully sent.');
-                return $this->redirect($this->generateUrl('kap_contact'));
-            }
-      }
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+
+            // SEND EMAIL TO AUTHORIZE PERSON
+            $formData = $form->getData();
+
+            $contactUsEmail = \Swift_Message::newInstance()
+                    ->setSubject("Contact Us: ".$formData['subject'])
+                    ->setFrom($formData['email'])
+                    ->setTo($this->container->getParameter('contact_us_email'))
+                    ->setBody($formData['message'])
+                    ->setContentType('text/html');
+            $this->container->get('mailer')->send($contactUsEmail);
+
+            // REDIRECT TO CONTACT US PAGE
+            $this->get('session')->getFlashBag()->add('success', 'Your request has been successfully sent.');
+            return $this->redirect($this->generateUrl('kap_contact'));
+        }
+      
        return $this->render('KapContactBundle:Contact:index.html.twig', array(
            'form' => $form->createView()
        ));
